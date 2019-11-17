e58fce22ef2a4f77f193d7f7d783f0d1aa5de9fc
Daniel Althof <EMAIL>
2019-11-17 22:02:02 +0000

    adding Vundle to vim script

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -1,7 +1,9 @@
 # Backups, swaps and undos are stored here.
 mkdir -p $DOTFILES/caches/vim
+# Install Vundle 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
 
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
-  vim +PlugUpgrade +PlugUpdate +qall
+	vim +PluginInstall +qall
 fi
