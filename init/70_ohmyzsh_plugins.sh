28951e2edce3fdaa7831ffdc50e13b95cf97ddbd
Daniel Althof <EMAIL>
2019-11-18 21:30:24 +0000

    some modifications to zshell plugins script

--- init/70_ohmyzsh_plugins.sh
+++ init/70_ohmyzsh_plugins.sh
@@ -1,13 +1,12 @@
-
 # Installing theme for zshell
   if [[ -d "$HOME/.oh-my-zsh/custom/plugins" ]]; then
     e_header "Installing zshell plugins"
     (
-cd $HOME/.oh-my-zsh/custom/plugins
-git clone https://github.com/bhilburn/powerlevel9k.git
-git clone https://github.com/zsh-users/zsh-autosuggestions
-git clone https://github.com/zdharma/fast-syntax-highlighting.git
-git clone https://github.com/djui/alias-tips.git
+        cd $HOME/.oh-my-zsh/custom/plugins
+        git clone https://github.com/bhilburn/powerlevel9k.git
+        git clone https://github.com/zsh-users/zsh-autosuggestions
+        git clone https://github.com/zdharma/fast-syntax-highlighting.git
+        git clone https://github.com/djui/alias-tips.git
     )
   fi
 
