0bb7e9d75f3463f23c90ad9a2aaed4f101a96999
Daniel Althof <EMAIL>
2020-04-18 23:49:44 +0000

    correcting some stuff

--- init/52_git.sh
+++ init/52_git.sh
@@ -2,10 +2,10 @@
 e_header "Clonig git projects"
 e_header "--> visitor guide cms"
 cd /var/www/html
-  if [[ ! -d "visitorguide_cms" ]]; then
-    git clone ssh://bb:7998/hal/base.git visitorguide_cms
-    cd visitorguide_cms
-    composer install
-  fi
+if [[ ! -d "visitorguide_cms" ]]; then
+  git clone ssh://bb:7998/hal/base.git visitorguide_cms
+  cd visitorguide_cms
+  composer install
+fi
 
 
