ca0a48e624c99b3ce1209709b557e98c1ff88b2e
Daniel Althof <EMAIL>
2020-04-18 21:40:52 +0000

    adding git clone of visitorguide_cms and install composer

---
+++ init/70_git.sh
@@ -0,0 +1,11 @@
+#cloning git porjects
+e_header "Clonig git projects"
+e_header "--> visitor guide cms"
+cd /var/www/html
+  if [[ ! -d "visitorguide_cms" ]]; then
+    git clone ssh://bb:7998/hal/base.git visitorguide_cms
+    cd visitorguide_cms
+    composer install
+  fi
+
+
